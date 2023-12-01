c7fb0f16af5744f7df9e37e08f0ceffe152df5f5
MohammadAhmadKhader <mohammadkhaderr1999@gmail.com>
2023-12-01 02:44:36 +0200

    homework is done, waiting for go sign

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 import { readFileSync } from "fs";
-import config from "./config.json" assert { type: "json" };
 
+// Enter how many files you intend to read
 function readFiles() {
-    try {
-        const data = config.files;
-        data.forEach((e)=>{
-            // Removing the quotes from text
-            let filePath = JSON.stringify(e);
-            let newFilePath =  filePath.slice(1,-1);
-
-            let fileText = readFileSync(newFilePath);
-            let space = " ";
+        const configFile = readFileSync("config.json" , "utf8")
+        const filesNamesObj = JSON.parse(configFile).files;
+        filesNamesObj.forEach((e)=>{
+            try {
+            let textContent = readFileSync(e, {encoding:"utf-8"});
             let wordsCounter = 0;
-            let doesHaveAValidString = false;
-            for (let str of fileText) {
-                if(str >= 65 && str <= 90 || str >= 97 && str <= 122 ) {
-                    doesHaveAValidString = true;
+
+            textContent.split(" ").forEach((e)=>{
+                wordsCounter++;
+            })
+
+            console.log(`${e} : ${wordsCounter} words`);
+
+            } catch (error) {
+                try {
+                    errorCounter(error);
+                } catch (error) {
+                    console.log(`
+                    ##############################################################################################################################################################
+                    Error has occured during attempt to read files childs of files folder : ${error}
+                    ##############################################################################################################################################################`
+                    )
                 }
-                if(String.fromCharCode(str) === space){
-                    wordsCounter++;
-                };  
+                
             }
-            // This condition made in case there are no sapces and at least one letter was found
-            if(doesHaveAValidString && wordsCounter === 0){
-                wordsCounter = 1;
-            }
-            console.log(`${newFilePath} : ${wordsCounter} words`);
         })
-    } catch (error) {
-        console.log(`
-        ###############################################################################
-        Error has occured during attempt to read files childs of files folder : ${error}
-        ###############################################################################`)
-    }
+
+}
+
+// When there is an error occured this function will try to solve the error by attempting to read the file caused error by using "utf-16le" unicode
+function errorCounter(error) {
+    let uniCodeFileText = readFileSync(`./files/${error.path}`,"utf-16le");
+
+    let uniCodeWordsCounter = 0;
+    uniCodeFileText.split(" ").forEach((e)=>{
+        uniCodeWordsCounter++;
+    })
+
+    console.log(`${getFolderName("config.json",0)}/${error.path} : ${uniCodeWordsCounter} words`);
+}
+
+// Give it a config.json/any Json file and the index of the file name inside it and or Object name and will return it!
+let getFolderName = (JSONOfTheFolder , fileNumber)=>{
+    const configFile = readFileSync(`${JSONOfTheFolder}` , "utf8")
+    const folderName = Object.keys(JSON.parse(configFile))[fileNumber];
+
+    return folderName;
 }
 
 readFiles();
